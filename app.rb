8c6c0c1a2704bb5acf8ad3a2a2f31e3564b46c14
Xavier Riley <xavriley@hotmail.com>
2016-08-19 16:37:03 +0100

    Remove lapack so a deploy will work

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
-require 'numru/lapack'
+#require 'numru/lapack'
 require 'sinatra'
 
 get '/' do
-  jobz = "V"
-  range = "I"
-  uplo = "U"
-  a = NArray[[1,2],[2,3]]
-  vl =  vu = 0 # not be used in this example
-  il = 1
-  iu = 2
-  abstol = 0.0
+  "Hello, world"
+  # jobz = "V"
+  # range = "I"
+  # uplo = "U"
+  # a = NArray[[1,2],[2,3]]
+  # vl =  vu = 0 # not be used in this example
+  # il = 1
+  # iu = 2
+  # abstol = 0.0
 
-  m, w, z, isuppz, work, iwork, info, a = NumRu::Lapack.dsyevr(jobz, range, uplo, a, vl, vu, il, iu, abstol)
-  "Hello ruby lapack #{[m,w,z,isuppz,work,iwork,info,a].inspect}"
+  # m, w, z, isuppz, work, iwork, info, a = NumRu::Lapack.dsyevr(jobz, range, uplo, a, vl, vu, il, iu, abstol)
+  # "Hello ruby lapack #{[m,w,z,isuppz,work,iwork,info,a].inspect}"
 end
